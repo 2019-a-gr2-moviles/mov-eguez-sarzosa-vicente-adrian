281195778d0b8a2b3a4c89ba8af26b76ddae9d00
Adrian Eguez <eadepto@hotmail.com>
2019-06-27 16:04:04 -0500

    cambios

--- 03-Android/app/src/main/java/com/example/myapplication/ConexionHttpActivity.kt
+++ 03-Android/app/src/main/java/com/example/myapplication/ConexionHttpActivity.kt
@@ -7,6 +7,10 @@ import com.beust.klaxon.Klaxon
 import java.lang.Exception
 import java.util.*
 
+import com.github.kittinunf.result.Result.Failure
+import com.github.kittinunf.result.Result.Success
+import com.github.kittinunf.fuel.httpGet
+
 class ConexionHttpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,28 +42,67 @@ class ConexionHttpActivity : AppCompatActivity() {
 
             empresaInstancia?.forEach {
 
-                Log.i("http",
-                    "Nombre ${it.nombre}")
+                Log.i(
+                    "http",
+                    "Nombre ${it.nombre}"
+                )
 
-                Log.i("http",
-                    "Id ${it.id}")
+                Log.i(
+                    "http",
+                    "Id ${it.id}"
+                )
 
-                Log.i("http",
-                    "Fecha ${it.fechaCreacion}")
+                Log.i(
+                    "http",
+                    "Fecha ${it.fechaCreacion}"
+                )
 
                 it.usuariosDeEmpresa.forEach {
-                    Log.i("http",
-                        "Nombre ${it.nombre}")
-                    Log.i("http",
-                        "FK ${it.fkEmpresa}")
+                    Log.i(
+                        "http",
+                        "Nombre ${it.nombre}"
+                    )
+                    Log.i(
+                        "http",
+                        "FK ${it.fkEmpresa}"
+                    )
                 }
 
             }
-        } catch (e:Exception){
-            Log.i("http","${e.message}")
-            Log.i("http",
-                "Error instanciando la empresa")
+        } catch (e: Exception) {
+            Log.i("http", "${e.message}")
+            Log.i(
+                "http",
+                "Error instanciando la empresa"
+            )
         }
+
+
+        val url = "http://172.31.104.99:1337/empresa/1"
+
+
+        url
+            .httpGet()
+            .responseString { request, response, result ->
+                when (result) {
+                    is Failure -> {
+                        val ex = result.getException()
+                        Log.i("http", "Error: ${ex.message}")
+                    }
+                    is Success -> {
+                        val data = result.get()
+                        Log.i("http", "Data: ${data}")
+
+                        val empresaParseada = Klaxon()
+                            .parse<Empresa>(data)
+                        if (empresaParseada != null) {
+                            Log.i("http"," iiiiiiiiiiiiiiiiiiii ")
+                            Log.i("http","${empresaParseada.nombre} ")
+                            Log.i("http","${empresaParseada.id} ")
+                        }
+                    }
+                }
+            }
 
 
     }
